25dabdf43de5cbac22be24558d484b24848b35e9
Gurunath123 <gurunathkulkarni96@gmail.com>
2019-02-17 18:42:41 +0530

    updated done

--- App.js
+++ App.js
@@ -11,10 +11,33 @@ import React, {Component} from 'react';
 import { View } from 'react-native';
 import  Login  from './src/component/Login';
 import Cardsection from './src/component/Cardsection';
+import firebase from 'firebase';
 import Card from './src/component/Card';
 import Button from './src/component/Button';
 
 class App extends Component {
+  state = {loggedIn:false }
+  componentWillMount() {
+  firebase.initializeApp({
+    apiKey: "AIzaSyD3Qg4Hc2gAaYg6PbGW1beZFT67WNK7zMw",
+    authDomain: "school-3051a.firebaseapp.com",
+    databaseURL: "https://school-3051a.firebaseio.com",
+    projectId: "school-3051a",
+    storageBucket: "school-3051a.appspot.com",
+    messagingSenderId: "120266050681"
+});
+
+firebase.auth().onAuthStateChanged((user) => {
+if(user){
+this.setState({loggedIn:true});
+}
+else
+this.setState({loggedIn:false});
+
+});
+ }
+
+
   render() {
     return(
       <View>
--- src/component/Login.js
+++ src/component/Login.js
@@ -1,36 +1,104 @@
-import React from 'react';
-import { View, Text,Image } from 'react-native';
+import React, {Component} from 'react';
+import { View, Text, Dimensions } from 'react-native';
+import Image from 'react-native-scalable-image';
 import Button from './Button';
 import Input from './Input';
 import Card from './Card';
 import Cardsection from './Cardsection';
+import firebase from 'firebase/app';
 const image = require('../../images/school.jpg');
 
-const Login = () =>
+class Login extends Component
 {
-    return(
+    constructor(props){
+        super(props);
+    
+        this.state = { mobileno: '' ,
+        password: '',
+        error: '',
+      loading: false};
+      }
+
+      onButtonPress(){
+        const { mobileno,password }=this.state;
+        this.setState({error: '',loading:true});
+        console.log('huyhhuybuyb ');
+      
+      firebase.auth().signInWithPhoneNumber(mobileno, firebase.auth.PhoneAuthProvider)
+      .then(this.onLoginSuccess.bind(this))
+      .catch(() => {
+       firebase.auth().createUserWithEmailAndPassword(mobileno, password)
+       .then(this.onLoginSuccess.bind(this))
+       .catch(this.onLoginFail.bind(this)); 
+         
+       });
+      
+      }
+
+      onLoginFail(){
+        this.setState({ error: 'Authentication Failed',loading: false });
+      }
+      onLoginSuccess(){
+      this.setState({
+      mobileno: '',
+      password: '',
+      loading: false,
+      error: ''
+      });
+    }
+
+    renderButton(){
+        if(this.state.loading)
+        {
+          return (
+              <Text>Done</Text>
+          );
+        }
+        return (
+      <Button onPress={this.onButtonPress.bind(this)}>Login</Button>
+        );
+      }
+
+    render(){
+        return(
        <Card>
-           <Cardsection>
+           <Cardsection style={{height:400}} >
             <Image
-            source ={ require('../../images/school.jpg')} 
+            width={Dimensions.get('window').width - 120}
+            source ={ require('../../images/School3.png')} 
             style ={ styles.imagestyle }/>
            </Cardsection>
+
            <Cardsection>
-               <Input/>
+           <Input
+           placeholder="XXXXXXXXX"
+           label="Mobile No"/>
            </Cardsection>
+
            <Cardsection>
-               <Button/>
-               
+           <Input
+           secureTextEntry
+           placeholder="password"
+           label="Password"/>
            </Cardsection>
+
+           <Cardsection>
+           {this.renderButton()}
+            </Cardsection>
        </Card>
     );
+ }
 }
 
 const styles ={
     imagestyle:{
-        height: 300,
-    flex: 1,
-    width: null
+        flex: 1,
+       marginTop:30,
+       marginLeft:30,
+       marginRight:30,
+       marginBottom:30     
+       
+
     }
 }
 
